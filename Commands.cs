09d6be371649facc1e76096a00a4c33e951845f0
izrik <izrik@izrik.com>
2014-03-11 15:22:05 -0500

    Update example2.

--- Commands.cs
+++ Commands.cs
@@ -257,17 +257,24 @@ namespace MetaphysicsIndustries.Ligra
 
         public static void Example2Command(string input, string[] args, LigraEnvironment env)
         {
-            Font f = env.Font;
 
             if (!env.Variables.ContainsKey("x")) env.Variables.Add("x", new Literal(0));
             if (!env.Variables.ContainsKey("y")) env.Variables.Add("y", new Literal(0));
 
             Expression expr;
 
-            var parser = new SolusParser();
-            expr = parser.GetExpression("unitstep((x*x+y*y)^0.5+2*(sin(t)-1))*cos(5*y+2*t)", env);
-            env.RenderItems.Add(new InfoItem("unitstep((x*x+y*y)^0.5+2*(sin(t)-1))*cos(5*y+2*t)", f));
-            env.RenderItems.Add(new Graph3dItem(expr, Pens.Black, Brushes.Green, -4, 4, -4, 4, -2, 6, "x", "y"));
+            var parser = new LigraParser();
+//            expr = parser.GetExpression("unitstep((x*x+y*y)^0.5+2*(sin(t)-1))*cos(5*y+2*t)", env);
+//            env.RenderItems.Add(new InfoItem("unitstep((x*x+y*y)^0.5+2*(sin(t)-1))*cos(5*y+2*t)", f));
+//            env.RenderItems.Add(new Graph3dItem(expr, Pens.Black, Brushes.Green, -4, 4, -4, 4, -2, 6, "x", "y"));
+
+            var input2 = "factorial(n) := if (n, n * factorial(n-1), 1)";
+            var input3 = "cos_taylor(x, n, sign) := if (n-8, sign * (x ^ n) / factorial(n) + cos_taylor(x, n+2, -sign), 0)";
+            var input4 = "cos2(x) := cos_taylor(x, 0, 1)";
+            parser.GetCommands(input2, env)[0](input2, null, env);
+            parser.GetCommands(input3, env)[0](input3, null, env);
+            parser.GetCommands(input4, env)[0](input4, null, env);
+
         }
 
         public static void PlotCommand(string input, string[] args, LigraEnvironment env, Expression[] exprs, VarInterval[] intervals)
