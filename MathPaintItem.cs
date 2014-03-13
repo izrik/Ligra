b461fcd2660c19269b34f3305fd9eaeb0d17994a
izrik <izrik@izrik.com>
2014-03-13 14:15:01 -0500

    Move RenderMathPaint from LigraControl to MathPaintItem.

--- MathPaintItem.cs
+++ MathPaintItem.cs
@@ -59,7 +59,7 @@ namespace MetaphysicsIndustries.Ligra
             if (_image == null || HasChanged(env))
             {
                 MemoryImage image =
-                    LigraControl.RenderMathPaintToMemoryImage(
+                    RenderMathPaintToMemoryImage(
                             _expression,
                             _horizontalCoordinate,
                             _verticalCoordinate,
@@ -98,5 +98,70 @@ namespace MetaphysicsIndustries.Ligra
             return new SizeF(_width, _height);
         }
 
+        static readonly SolusEngine _engine = new SolusEngine();
+
+        public static MemoryImage RenderMathPaintToMemoryImage(
+            Expression expression, 
+            string independentVariableX, 
+            string independentVariableY, 
+            int xStart, int xEnd,
+            int yStart, int yEnd,
+            SolusEnvironment env)
+        {
+            int xValues = xEnd - xStart + 1;
+            int yValues = yEnd - yStart + 1;
+
+            double[,] values = new double[xValues, yValues];
+
+            Expression prelimEval1;
+            Expression prelimEval2;
+
+            if (env.Variables.ContainsKey(independentVariableX))
+            {
+                env.Variables.Remove(independentVariableX);
+            }
+            if (env.Variables.ContainsKey(independentVariableY))
+            {
+                env.Variables.Remove(independentVariableY);
+            }
+
+            prelimEval1 = _engine.PreliminaryEval(expression, env);
+
+            int i;
+            int j;
+            double z;
+
+            MemoryImage image = new MemoryImage(xValues, yValues);
+            //image.Size = new Size(xValues, yValues);
+
+            for (i = 0; i < xValues; i++)
+            {
+                env.Variables[independentVariableX] = new Literal(i);
+                if (env.Variables.ContainsKey(independentVariableY))
+                {
+                    env.Variables.Remove(independentVariableY);
+                }
+
+                prelimEval2 = _engine.PreliminaryEval(prelimEval1, env);
+
+                for (j = 0; j < yValues; j++)
+                {
+                    env.Variables[independentVariableY] = new Literal(j);
+
+                    z = prelimEval2.Eval(env).Value;
+
+                    if (double.IsNaN(z))
+                    {
+                        z = 0;
+                    }
+
+                    image[j, i] = Color.FromArgb(255, Color.FromArgb((int)z));
+                    //values[i, j] = z;
+                }
+            }
+
+            image.CopyPixelsToBitmap();
+            return image;
+        }
     }
 }
