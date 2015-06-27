ccd77c7921eab616b05c4ffdb97d7e298160d7e8
izrik <izrik@izrik.com>
2015-06-27 10:38:56 -0500

    Add a read-only list class and interface.

---
+++ IReadOnlyList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MetaphysicsIndustries.Ligra
+{
+    public interface IReadOnlyList<T>
+    {
+        int IndexOf(T item);
+
+        T this [int index] { get; }
+
+        bool Contains(T item);
+
+        void CopyTo(T[] array, int arrayIndex);
+
+        int Count { get; }
+
+        IEnumerator<T> GetEnumerator();
+    }
+}
+
---
+++ ReadOnlyList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MetaphysicsIndustries.Ligra
+{
+    public class ReadOnlyList<T> : IReadOnlyList<T>
+    {
+        public ReadOnlyList(IList<T> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            _source = source;
+        }
+
+        readonly IList<T> _source;
+
+        #region IReadOnlyList implementation
+
+        public int IndexOf(T item)
+        {
+            return _source.IndexOf(item);
+        }
+
+        public bool Contains(T item)
+        {
+            return _source.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _source.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _source.GetEnumerator();
+        }
+
+        public T this[int index]
+        {
+            get { return _source[index]; }
+        }
+
+        public int Count
+        {
+            get { return _source.Count; }
+        }
+
+        #endregion
+    }
+}
+
