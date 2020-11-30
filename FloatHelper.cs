d3946b4d9a5867b9e9574b2e79f68da06cf06876
izrik <izrik@izrik.com>
2020-11-30 09:03:50 -0600

    Add some math utility types.

---
+++ FloatHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MetaphysicsIndustries.Ligra
+{
+    public static class FloatHelper
+    {
+        public static int RoundToInt(this float x)
+        {
+            return (int)Math.Round(x);
+        }
+    }
+}
