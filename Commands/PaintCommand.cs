239ffe1ca59fd82f889d2d8382fb4f13b76b9d55
izrik <izrik@izrik.com>
2021-06-17 16:39:01 -0500

    Add a docstring for paint.

--- Commands/PaintCommand.cs
+++ Commands/PaintCommand.cs
@@ -14,7 +14,28 @@ namespace MetaphysicsIndustries.Ligra.Commands
         private readonly Expression _expr;
         private readonly VarInterval _interval1;
         private readonly VarInterval _interval2;
-        
+
+        public override string DocString =>
+@"Mathpaint
+  Color the pixels of an image using an expression.
+
+paint <expr> for <interval1>, <interval2>
+
+  expr
+    The expression to evaluate for each pixel. The result is interpreted as a
+    24-bit integer, with the blue channel in the lowest 8 bits, the green
+    channel in the middle 8 bits, and the red channel in the highest 8 bits.
+  interval1
+    An integer interval, of the form ""<var>=[<start>..<end>]"". It defines
+    <var> as a variable within <expr>, to be assigned the successive values of
+    the interval from <start> to <end>, inclusive. This interval determines the
+    horizontal dimension of the image.
+  interval2
+    An integer interval, of the form ""<var>=[<start>..<end>]"". It defines
+    <var> as a variable within <expr>, to be assigned the successive values of
+    the interval from <start> to <end>, inclusive. This interval determines the
+    vertical dimension of the image.
+";
         public override void Execute(string input, string[] args, LigraEnvironment env)
         {
             // holy smokes, this is *hideous*
