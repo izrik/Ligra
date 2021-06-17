60ad2e9021837e95e2907a844793f090358fe014
izrik <izrik@izrik.com>
2021-06-17 15:15:00 -0500

    Remove cruft.

--- HelpItem.cs
+++ HelpItem.cs
@@ -34,41 +34,7 @@ Available Topics:
   Commands: help, clear, vars, delete, history, example
 
 Type ""help list"" to see the current environment";
-            //_helpLookups["ligra"] = HelpStrings.ligra;
-            //_helpLookups["Functions"] = HelpStrings.functions;
-            //_helpLookups["cos"] = "The cosine function\n  cos(x)\n\nReturns the cosine of x.";
-            //_helpLookups["sin"] = "The sine function\n  sin(x)\n\nReturns the sine of x.";
-            //_helpLookups["tan"] = HelpStrings.tan;
-            //_helpLookups["sec"] = HelpStrings.sec;
-            //_helpLookups["csc"] = "The cosecant function\n  csc(x)\n\nReturns the cosecant of x, which is equal to 1 / sin(x).";
-            //_helpLookups["cot"] = "The cotangent function\n  cot(x)\n\nReturns the cotangent of x, which is equal to 1 / tan(x).";
-            //_helpLookups["acos"] = "The arccosine function\n  acos(x)\n\nReturns the arccosine of x. That is, if cos(y) = x, then acos(x) = y.";
-            //_helpLookups["asin"] = "The arcsine function\n  asin(x)\n\nReturns the arcsine of x. That is, if sin(y) = x, then asin(x) = y.";
-            //_helpLookups["atan"] = "The arctangent function\n  atan(x)\n\nReturns the arctangent of x. That is, if tan(y) = x, then atan(x) = y.";
-            //_helpLookups["atan2"] = "The atan2 function\n  atan(y, x)\n\nReturns the arctangent of y/x.";
-            //_helpLookups["asec"] = "The arcsecant function\n  asec(x)\n\nReturns the arcsecant of x. That is, if sec(y) = x, then asec(x) = y.";
-            //_helpLookups["acsc"] = "The arccosecant function\n  acsc(x)\n\nReturns the arccosecant of x. That is, if csc(y) = x, then acsc(x) = y.";
-            //_helpLookups["acot"] = "The arccotangent function\n  acot(x)\n\nReturns the arccotangent of x. That is, if cot(y) = x, then acot(x) = y.";
-            //_helpLookups["ln"] = "The natural logarithm function";
-            //_helpLookups["log"] = HelpStrings.log;
-            //_helpLookups["log2"] = HelpStrings.log2;
-            //_helpLookups["log10"] = HelpStrings.log10;
-            //_helpLookups["sqrt"] = "square root";
-            //_helpLookups["int"] = HelpStrings.int;
-            //_helpLookups["abs"] = "The absolute value function\n  abs(x)\n\nReturns the absolute value of x, x for (x >= 0) and -x for (x < 0).";
-            //_helpLookups["rand"] = HelpStrings.rand;
-            //_helpLookups["ceil"] = "The ceiling function\n  ceil(x)\n\nReturns the lowest integer that is greater than or equal to x.\n";
-            //_helpLookups["u"] = "unit step function";
-            //_helpLookups["Operators"] = HelpStrings.operators;
-            //_helpLookups["+"] = HelpStrings.+;
-            //_helpLookups["-"] = HelpStrings.-;
-            //_helpLookups["*"] = HelpStrings.*;
-            //_helpLookups["/"] = HelpStrings./;
-            //_helpLookups["Special"] = HelpStrings.special;
-            //_helpLookups["derive"] = "The derive operator\n  derive(f(x), x)\n\nReturns the derivative of f(x) with respect to x.";
-            //_helpLookups["Plotting"] = HelpStrings.plotting;
             _helpLookups["t"] = "default time variable";
-            //_helpLookups["floor"] = "The floor function\n  floor(x)\n\nReturns the highest integer that is less than or equal to x.";
         }
 
         public HelpItem(LFont font, LigraEnvironment env, string topic = "help")
