eaa87e3bbf6c9c480658985318a3a2fdc53d6249
izrik <izrik@izrik.com>
2021-06-17 10:39:26 -0500

    Provide a string representation of an interval.

--- VarInterval.cs
+++ VarInterval.cs
@@ -6,6 +6,17 @@ namespace MetaphysicsIndustries.Ligra
     {
         public string Variable;
         public Interval Interval;
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{0} {1} {2} {3} {4}",
+                Interval.LowerBound,
+                (Interval.OpenLowerBound ? "<" : "<="),
+                Variable,
+                (Interval.OpenUpperBound ? "<" : "<="),
+                Interval.UpperBound);
+        }
     }
 }
 
