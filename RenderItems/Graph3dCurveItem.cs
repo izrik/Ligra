59a5cc45d3bdb85c7da7ef7cea7546ee4d4e4f37
izrik <izrik@izrik.com>
2021-11-09 23:39:37 -0600

    Re-format. Adjust comment.

--- RenderItems/Graph3dCurveItem.cs
+++ RenderItems/Graph3dCurveItem.cs
@@ -122,7 +122,8 @@ namespace MetaphysicsIndustries.Ligra.RenderItems
             }
         }
 
-        public static void EvaluateGraph(ref Vector3[] points,
+        public static void EvaluateGraph(
+            ref Vector3[] points,
             Expression expr,
             LigraEnvironment env,
             VarInterval interval,
@@ -141,10 +142,12 @@ namespace MetaphysicsIndustries.Ligra.RenderItems
             {
                 float x = varMin + delta * i;
                 env.SetVariable(varName, new Literal(x));
+
                 var vv = expr.Eval(env);
                 if (!vv.IsConcrete)
                     // EvaluationException ?
-                    throw new OperandException("Value is not concrete");
+                    throw new OperandException(
+                        "Value is not concrete");
 
                 Vector3 pt;
                 if (vv.IsVector(null))
@@ -155,7 +158,7 @@ namespace MetaphysicsIndustries.Ligra.RenderItems
                             "Value is not a 3-vector");
                     var vvv = vv.ToVector();
                     // TODO: check for NaN
-                    // TODO: ensure vvv[0] and vvv[1] are scalars
+                    // TODO: ensure components of vvv are scalars
                     pt = new Vector3(
                         vvv[0].ToNumber().Value,
                         vvv[1].ToNumber().Value,
